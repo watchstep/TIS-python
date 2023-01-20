99ec419ed3b866d2932327623ae2ab69cccd352c
kimjuii <cathy033077@gmail.com>
2023-01-20 23:56:39 +0900

    2023-01-20

--- BAEKJOON/silver3/1463.py
+++ BAEKJOON/silver3/1463.py
@@ -1,10 +1,31 @@
 # 1로 만들기
 import sys
+import math
 input = sys.stdin.readline
 
 N = int(input())
-seq_3 = [0] + [0]*10**6
-seq_2 = [0] + [0]*10**6
+# index를 1부터 시작하도록 앞에 0 추가
+seq = [0] + [0]*10**6
 
-for i in range(1, N+1):
-  if i
+for i in range(2, N+1):
+  seq[i] = seq[i-1] + 1
+  if i % 2 == 0:
+    seq[i] = min(seq[i], seq[i//2]+1)
+  if i % 3 == 0:
+    seq[i] = min(seq[i], seq[i//3]+1)
+  
+print(seq[N])
+
+      
+'''
+1 : 0
+2 : 1
+3 : 1
+4 : 2 (2*2)
+5 : 3 (4+1)
+6 : 2 (3*2)
+7 : 3 (6+1)
+8 : 3 (4*2)
+9 : 2 (3*3)
+10: 3 (9+1)
+'''
