3b80d8758d1d48cfa4dea760b1b2bc801e8fe59a
kimjuii <cathy033077@gmail.com>
2023-02-10 22:16:44 +0900

    2021-02-10

--- BAEKJOON/gold5/14719.py
+++ BAEKJOON/gold5/14719.py
@@ -1,1 +1,21 @@
 # 빗물
+import sys
+input = sys.stdin.readline
+
+H, W = map(int, input().split())
+block_h = [*map(int, input().split())]
+
+rain = 0
+  
+
+for i in range(1, W-1):
+  left = max(block_h[:i])
+  right = max(block_h[i+1:])
+  
+  tmp = min(left, right)
+  
+  # 
+  if tmp > block_h[i]:
+    rain += tmp - block_h[i]
+    
+print(rain)
